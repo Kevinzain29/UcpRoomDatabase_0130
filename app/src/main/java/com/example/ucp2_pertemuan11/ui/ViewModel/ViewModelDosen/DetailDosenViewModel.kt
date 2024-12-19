9fc448629ececb742cbb7f1a0dc93174119e08a9
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-19 15:50:46 +0700

    add data class 'DetailUiState' on DetailDosenViewModel

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DetailDosenViewModel.kt
@@ -1,3 +1,15 @@
 package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen
 
 
+data class DetailUiState(
+    val detailUiEvent: DosenEvent = DosenEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == DosenEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != DosenEvent()
+}
