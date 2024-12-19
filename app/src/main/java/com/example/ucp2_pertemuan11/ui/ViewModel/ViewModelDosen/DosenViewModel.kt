c302c4284430171af61a2e4d73927934e59bc9c9
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-19 15:27:44 +0700

    add class 'DosenViewModel, function validateFields'

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DosenViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.ucp2_pertemuan11.data.Entity.Dosen
+import com.example.ucp2_pertemuan11.repository.RepositoryDosen
+
+class DosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel() {
+
+    var uiState by mutableStateOf(DosenUIState())
+
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent,
+        )
+    }
+    //validasi data input pengguna
+    private fun validateFields(): Boolean {
+        val event = uiState.dosenEvent
+        val errorState = FormErrorState(
+            Nidn = if (event.Nidn.isNotEmpty()) null else "Nidn tidak boleh kosong",
+            Nama = if (event.Nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            JenisKelamin = if (event.JenisKelamin.isNotEmpty()) null else "JenisKelamin tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
+
 
 data class DosenUIState(
     val dosenEvent: DosenEvent = DosenEvent(),
