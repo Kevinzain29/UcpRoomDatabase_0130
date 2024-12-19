28673835ec00bd829ce212b34e1bff25e8e10442
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-19 15:13:02 +0700

    add data class 'FormErrorState, DosenUIState'

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DosenViewModel.kt
@@ -2,12 +2,30 @@ package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen
 
 import com.example.ucp2_pertemuan11.data.Entity.Dosen
 
+data class DosenUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null
+)
+
+//untuk memvalidasi
+data class FormErrorState(
+    val Nidn: String? = null,
+    val Nama: String? = null,
+    val JenisKelamin: String? = null,
+){
+    fun isValid(): Boolean {
+        return Nidn == null && Nama == null && JenisKelamin == null
+    }
+}
+
+//menyimpan input form ke dalam entity
 fun DosenEvent.toDosenEntity(): Dosen = Dosen(
     Nidn = Nidn,
     Nama = Nama,
     JenisKelamin = JenisKelamin
 )
-
+//untuk menyimpan inputan dari textfield ke variabel/menghandle textfield
 data class DosenEvent(
     val Nidn: String = "",
     val Nama: String = "",
