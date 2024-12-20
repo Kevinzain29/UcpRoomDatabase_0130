a60a9ec85972484fde8708339873fd7a2073a194
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-20 23:39:24 +0700

    add 'data class FormErrorState, data class MataKuliahUIState'

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelMataKuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelMataKuliah/MataKuliahViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelMataKuliah
 
 import com.example.ucp2_pertemuan11.data.Entity.MataKuliah
+import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.DosenEvent
 
+data class MataKuliahUIState(
+    val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null
+)
+
+//untuk memvalidasi
+data class FormErrorState(
+    val Kode: String? = null,
+    val Nama: String? = null,
+    val SKS: String? = null,
+    val Semester: String? = null,
+    val Jenis: String? = null,
+    val DosenPengampu: String? = null
+){
+    fun isValid(): Boolean {
+        return Kode == null && Nama == null && SKS == null &&
+                Semester == null && Jenis == null && DosenPengampu == null
+    }
+}
 
 //menyimpan input form ke dalam entity
 fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
