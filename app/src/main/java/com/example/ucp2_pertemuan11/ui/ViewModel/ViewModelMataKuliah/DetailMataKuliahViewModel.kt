132f2c3704348c9873ffbba6c14d5ab0f362a587
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-21 01:35:27 +0700

    add class 'DetailMataKuliahViewModel' on DetailMataKuliahViewModel

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelMataKuliah/DetailMataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelMataKuliah/DetailMataKuliahViewModel.kt
@@ -1,6 +1,54 @@
 package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelMataKuliah
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pertemuan11.data.Entity.MataKuliah
+import com.example.ucp2_pertemuan11.repository.RepositoryMataKuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailMataKuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMataKuliah: RepositoryMataKuliah
+) : ViewModel() {
+    private val _Kode: String = checkNotNull(savedStateHandle[DestinasiDetailMataKuliah.Kode])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMataKuliah.getMataKuliah(_Kode)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class DetailUiState(
     val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
