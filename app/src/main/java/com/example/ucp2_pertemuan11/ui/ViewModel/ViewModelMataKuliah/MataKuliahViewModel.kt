0cb34f60ff6f8e3df10bea25a0230c7681e78e86
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-21 00:00:10 +0700

    add 'class MataKuliahViewModel, function updateState, function validateFields'

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelMataKuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelMataKuliah/MataKuliahViewModel.kt
@@ -1,7 +1,38 @@
 package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelMataKuliah
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.ucp2_pertemuan11.data.Entity.MataKuliah
-import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.DosenEvent
+import com.example.ucp2_pertemuan11.repository.RepositoryMataKuliah
+import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.FormErrorState
+
+class MataKuliahViewModel(private val repositoryMataKuliah: RepositoryMataKuliah) : ViewModel() {
+
+    var uiState by mutableStateOf(MataKuliahUIState())
+
+    fun updateState(mataKuliahEvent: MataKuliahEvent) {
+        uiState = uiState.copy(
+            mataKuliahEvent = mataKuliahEvent,
+        )
+    }
+
+    //validasi data input pengguna
+    private fun validateFields(): Boolean {
+        val event = uiState.mataKuliahEvent
+        val errorState = FormErrorState(
+            Kode = if (event.Kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            Nama = if (event.Nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            SKS = if (event.SKS.isNotEmpty()) null else "SKS tidak boleh kosong",
+            Semester = if (event.Semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            Jenis = if (event.Jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
+            DosenPengampu = if (event.DosenPengampu.isNotEmpty()) null else "DosenPengampu tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
 
 data class MataKuliahUIState(
     val mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
