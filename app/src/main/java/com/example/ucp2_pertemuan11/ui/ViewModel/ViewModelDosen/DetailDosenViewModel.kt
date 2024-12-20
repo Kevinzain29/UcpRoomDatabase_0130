12d6f4c0e2e42078fdd54a978f8467f6561ca54d
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-20 13:26:17 +0700

    add class 'DetailDosenViewModel' on DetailDosenViewModel

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelDosen/DetailDosenViewModel.kt
@@ -1,6 +1,55 @@
 package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2_pertemuan11.data.Entity.Dosen
+import com.example.ucp2_pertemuan11.repository.RepositoryDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailDosenViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDosen: RepositoryDosen
+) : ViewModel() {
+    private val _Nidn: String = checkNotNull(savedStateHandle[DestinasiDetailDosen.Nidn])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryDosen.getDosen(_Nidn)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            )
+        )
+}
+
 
 
 data class DetailUiState(
