aafa4d971fcf1bd6c9b4c277a72b57bc68f21b77
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-21 00:32:05 +0700

    add file DetailMataKuliahViewModel: 'data class DetailUiState'

---
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/ViewModel/ViewModelMataKuliah/DetailMataKuliahViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelMataKuliah
+
+data class DetailUiState(
+    val detailUiEvent: MataKuliahEvent = MataKuliahEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MataKuliahEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MataKuliahEvent()
+} //data class untuk menampung data yang akan ditampilkan di UI
