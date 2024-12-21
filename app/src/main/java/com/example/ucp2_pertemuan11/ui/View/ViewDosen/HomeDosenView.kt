756e06dd18974a61cbfb50788c92818c84e2a79c
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-21 12:18:10 +0700

    fix error on HomeDosenView, add function 'BodyHomeDosenView'

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/View/ViewDosen/HomeDosenView.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/View/ViewDosen/HomeDosenView.kt
@@ -1,21 +1,38 @@
 package com.example.ucp2_pertemuan11.ui.View.ViewDosen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.HomeDosenViewModel
+import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.HomeUiState
 import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.PenyediaDosenViewModel
 import com.example.ucp2_pertemuan11.ui.costumwidget.TopAppBar
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeDosenView(
-    viewModel: HomeDosenView = viewModel(factory = PenyediaDosenViewModel.Factory),
+    viewModel: HomeDosenViewModel = viewModel(factory = PenyediaDosenViewModel.Factory),
     onAddDosen: () -> Unit = {},
     onDetailClick: (String) -> Unit = {},
     modifier: Modifier = Modifier
@@ -52,5 +69,60 @@ fun HomeDosenView(
             },
             modifier = Modifier.padding(innerpadding)
         )
+    }
+}
+
+@Composable
+fun BodyHomeDosenView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeUiState.isLoading -> {
+            //menampilkan indikator loding
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.isError -> {
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage.let { Message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(Message)
+                    }
+                }
+            }
+        }
+        homeUiState.listDosen.isEmpty() ->{
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data mahasiswa",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            ListDOSEN(
+                listDosen = homeUiState.listDosen,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
     }
 }
