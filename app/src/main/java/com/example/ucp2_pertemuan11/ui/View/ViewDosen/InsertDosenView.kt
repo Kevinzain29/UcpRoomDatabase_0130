286bef632cc08e76d02f685f3b738060df7f2892
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-21 17:47:58 +0700

    add function 'InsertBodyDosen' on InsertDosenView

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/View/ViewDosen/InsertDosenView.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/View/ViewDosen/InsertDosenView.kt
@@ -1,18 +1,24 @@
 package com.example.ucp2_pertemuan11.ui.View.ViewDosen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.DosenEvent
+import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.DosenUIState
 import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.DosenViewModel
 import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.PenyediaDosenViewModel
 import com.example.ucp2_pertemuan11.ui.costumwidget.TopAppBar
@@ -70,6 +76,34 @@ fun InserDosenView(
                     onNavigate()
                 }
             )
+        }
+    }
+}
+
+@Composable
+fun InsertBodyDosen(
+    modifier: Modifier = Modifier,
+    onValueChange: (DosenEvent) -> Unit,
+    uiState: DosenUIState,
+    onClick: () -> Unit
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        FormDosen(
+            dosenEvent = uiState.dosenEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+
+            ) {
+            Text("Simpan")
         }
     }
 }
