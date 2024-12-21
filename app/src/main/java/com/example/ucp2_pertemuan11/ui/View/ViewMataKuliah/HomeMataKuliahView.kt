780e3877786ffc6eaa8ada477cefba037bc2563a
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-21 21:01:41 +0700

    add function 'ListMATAKULIAH, CardMK' on HomeMataKuliahView

--- app/src/main/java/com/example/ucp2_pertemuan11/ui/View/ViewMataKuliah/HomeMataKuliahView.kt
+++ app/src/main/java/com/example/ucp2_pertemuan11/ui/View/ViewMataKuliah/HomeMataKuliahView.kt
@@ -1,10 +1,22 @@
 package com.example.ucp2_pertemuan11.ui.View.ViewMataKuliah
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -24,7 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelDosen.HomeUiState
+import com.example.ucp2_pertemuan11.data.Entity.MataKuliah
 import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelMataKuliah.HomeMataKuliahViewModel
 import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelMataKuliah.HomeUiStateMataKuliah
 import com.example.ucp2_pertemuan11.ui.ViewModel.ViewModelMataKuliah.PenyediaMataKuliahViewModel
@@ -124,6 +136,115 @@ fun BodyHomeMataKuliahView(
                 },
                 modifier = modifier
             )
+        }
+    }
+}
+
+@Composable
+fun ListMATAKULIAH(
+    listMataKuliah: List<MataKuliah>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = listMataKuliah,
+            itemContent = { mk ->
+                CardMK(
+                    mk = mk,
+                    onClick = { onClick(mk.Kode) }
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun CardMK(
+    mk: MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+){
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ){
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.Nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.Kode,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.SKS,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Face, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.Semester,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.Jenis,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mk.DosenPengampu,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
         }
     }
 }
